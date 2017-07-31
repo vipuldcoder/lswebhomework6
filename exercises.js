ca46c73220ea3145bb316e5f24790a2ee3794d59
vipul Aggarwal <vipul.captureart@gmail.com>
2017-07-31 07:44:18 +0530

    added a new comment

--- exercises.js
+++ exercises.js
@@ -17,12 +17,18 @@ console.log(subtractFive(13));
 function areSameLength(str1, str2) {
   //return true if the two strings have the same length
   //otherwise return false
+   if(str1.length === str2.length){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 function areEqual(x, y) {
   //return true if x and y are the same
   //otherwise return false
-  return if (x == y) {
+  return if (x === y) {
     console.log(true);
   } else {
     console.log(false);
@@ -32,7 +38,7 @@ function areEqual(x, y) {
 function lessThanNinety(num) {
   //return true if num is less than ninety
   //otherwise return false
-  return if(num <= 90) {
+  return if(num < 90) {
     console.log(true);
   } else {
     console.log(false);
@@ -106,48 +112,57 @@ function cube(num) {
 
 function raiseToPower(num, exponent) {
   //raise num to whatever power is passed in as exponent
-  return num ** exponent;
+  return Math.pow(num,exponent);  
 }
 
 function roundNumber(num) {
   //round num and return it
+  return Math.round(num);
 }
 
 function roundUp(num) {
   //round num up and return it
+  return Math.ceil(num);
 }
 
 function addExclamationPoint(str) {
   //add an exclamation point to the end of str and return the new string
   //'hello world' -> 'hello world!'
+  return str + '!';
 }
 
 function combineNames(firstName, lastName) {
   //return firstName and lastName combined as one string and separated by a space.
   //'Lambda', 'School' -> 'Lambda School'
+  return firstName + ' ' + lastName;
 }
 
 function getGreeting(name) {
   //Take the name string and concatenate other strings onto it so it takes the following form:
   //'Sam' -> 'Hello Sam!'
+  return 'Hello ' + name + '!';
 }
 
 //If you can't remember these area formulas then head over to Google or look at the test code.
 
 function getRectangleArea(length, width) {
   //return the area of the rectangle by using length and width
+  return length*width;
 }
 
 function getTriangleArea(base, height) {
   //return the area of the triangle by using base and height
+  return (base*height)/2;
 }
 
 function getCircleArea(radius) {
   //return the rounded area of the circle given the radius
+  return Math.round(Math.PI * Math.pow(radius,2));
 }
 
 function getRectangularPrismVolume(length, width, height) {
   //return the area of the 3D rectangular prism given the length, width, and height
+  return width*length*height;
 }
 
 //Do not modify code below this line.
